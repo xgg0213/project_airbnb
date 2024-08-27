19877d60363a1cdc161b0ccfdc69843018c668d3
xgg0213 <m.eringan@gmail.com>
2024-08-26 17:11:19 -0700

    authenticateMe phase 1 completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require("./api");
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -8,13 +9,15 @@ const router = express.Router();
 // });
 
 // Add a XSRF-TOKEN cookie
-router.get(" ", (req, res) => {
+router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
   res.status(200).json({
     'XSRF-Token': csrfToken
   });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
 
