6cce03c9a59a898f4bea4f1e98d71849bab56809
xgg0213 <m.eringan@gmail.com>
2024-09-06 21:15:34 -0700

    modified the REAME.md

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
+const bookingsRouter = require('./bookings.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 
@@ -17,6 +18,7 @@ router.use('/users', usersRouter);
 // adding spots/reviews/bookings/images
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
