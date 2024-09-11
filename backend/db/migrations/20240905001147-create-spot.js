fc54b2d27a7a15eb4c86f9347c2928d3b3b00089
xgg0213 <m.eringan@gmail.com>
2024-09-11 16:23:51 -0700

    changing table relation name

--- backend/db/migrations/20240905001147-create-spot.js
+++ backend/db/migrations/20240905001147-create-spot.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Owners',
+          model: 'Owner',
           key: 'id'
         }
       },
