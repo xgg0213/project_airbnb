d45e1353058a3fe6f6dd2d6a015709decca6d0d3
xgg0213 <m.eringan@gmail.com>
2024-12-02 00:46:08 -0800

    initial frontend deploy commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,24 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
+
+// below is what is initially input - updated code reflected since line 23
 // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   plugins: [
+//     react(),
+//     eslint({
+//       lintOnStart: true,
+//       failOnError: mode === "production"
+//     })
+//   ],
+//   // To automatically open the app in the browser whenever the server starts,
+//   // uncomment the following lines:
+//   // server: {
+//   //   open: true
+//   // }
+// }));
+
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
@@ -11,9 +28,9 @@ export default defineConfig(({ mode }) => ({
       failOnError: mode === "production"
     })
   ],
-  // To automatically open the app in the browser whenever the server starts,
-  // uncomment the following lines:
-  // server: {
-  //   open: true
-  // }
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000'
+    },
+  }
 }));
